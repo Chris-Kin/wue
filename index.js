fb966813240336e12f5ddc8413a589ea7b94c935
mingyan.yu <mingyan.yu@wormpex.com>
2019-05-30 00:16:12 +0800

    feat:fragment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var app = document.querySelector('#app');
-
 function createFragment(rootDOM) {
   var fragment = document.createDocumentFragment();
   var child;
   while (child = rootDOM.firstChild) {
-    fragment.appendChild(child);
+    // 注意：当child被append到createDocumentFragment时，会自动从原有节点中删除！！
+    fragment.append(child);
   }
   return fragment;
 }
