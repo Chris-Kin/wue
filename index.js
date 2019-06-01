d9f63e685a2dea4fb6c8fc133987007145e3fc44
mingyan.yu <mingyan.yu@wormpex.com>
2019-06-01 11:26:15 +0800

    feat:初级compile

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var app = document.querySelector('#app');
-function createFragment(rootDOM) {
-  var fragment = document.createDocumentFragment();
-  var child;
-  while (child = rootDOM.firstChild) {
-    // 注意：当child被append到createDocumentFragment时，会自动从原有节点中删除！！
-    fragment.append(child);
-  }
-  return fragment;
-}
-
-var newDOM = createFragment(app);
-
-app.appendChild(newDOM);
+var app = Vue({
+  el: '#app',
+  data: {
+    name: 'alax',
+  },
+})
